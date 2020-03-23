7303643fc433b637895f10239d030e436832fb46
karpishyuliya <yuliakarpish@gmail.com>
2020-03-23 12:57:26 +0100

    6. Displainq a Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,13 +9,16 @@ class Quiz extends Component {
         super(props)
         this.state = {quiz_position: 1}
     }
+    showNextQuestion(){
+        this.state.quiz_position =this.state.quiz_position + 1
+    }
 
     render(){
     const isQuizEnd = ((this.state.quiz_position-1) === quizData.quiz_questions.length)
     
         return (
             <div>
-                {isQuizEnd? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}                
+                {isQuizEnd? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}                
             </div>
         )
     }
